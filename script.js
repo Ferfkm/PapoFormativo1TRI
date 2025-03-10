199f83774f29995116b4d74e1e7106403e3da9fd
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2025-03-10 14:47:51 -0300

    function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     
-    aumentaFonteBotao.addEventListener('click', functio(){
+    aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte +=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
